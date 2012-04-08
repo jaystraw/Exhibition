f75c8b8788b1745bedd5f64833ca0f7ff2026bdd
Jay Straw <jay@jaystraw.info>
2012-04-08 12:51:51 -0800

    exhibition.js models are operational. docs updated

--- exhibition.js
+++ exhibition.js
@@ -37,45 +37,23 @@ var Exhibition = function(){
 //	ENVIRONMENT
 //
 
-	var requestHandler = function( req ,rsp ){
-		// parse request path
-		var path = url.parse(req.url);
-		// decide what to do
-		// SCRIPTS
-		if( path.pathname === '/script' || path.pathname.substr( 0 ,8 ) === '/script/' ){
-			rsp.end('/* nothing yet to see here */');
-		}
-		else
-		// STYLES
-		if( path.pathname === '/style' || path.pathname.substr( 0,7 ) === '/style/' ){
-			rsp.end('/* nothing to yet here see */');
-		}
-		else
-		// EVERYTHING ELSE
-		{
-			rsp.writeHead(
-				200
-				,{'Content-Type':'text/html'}
-			);
-			var r = [];
-
-			r.push('<!DOCTYPE html>');
-			r.push('<html lang="en">');
-			r.push('<head>');
-			r.push('<title>EXHIBITION</title>');
-			r.push('<link rel="stylesheet" type="text/css" href="/style" />');
-			r.push('<script type="text/javascript" src="/script"></script>');
-			r.push('</head>');
-			r.push('<body>');
-			r.push('<div class="exhibition">');
-			r.push('<div class="main-exhibit">');
-			
-			r.push('</div>')
-			r.push('</div>');
-			r.push('</body>');
-			r.push('</html>');
-			
-			rsp.end( r.join('\n') );
+	var requestHandler = new function(){
+		var registry = [];
+		this.register = function( handler ){
+			registry.push( handler );
+		};
+		this.do = function( req ,rsp ){
+			var path = url.parse(req.url,true);
+			for( var i = 0; i < registry.length; i++ ){
+				if( registry[i].call( this ,req ,rsp ,path ) === false ){
+					break;
+				}
+			}
+		};
+		this.createServer = function(){
+			var server = http.createServer( requestHandler.do.bind( this ) );
+			server.listen( port ,ip );
+			console.log('Started server on '+ip+':'+port+'...');
 		};
 	};
 
@@ -93,7 +71,7 @@ var Exhibition = function(){
 		//	DATA
 		//	Data is the raw stuff or media we want in our Exhibition
 		type: { type: String }	// mime type
-		,encoding: String	// base64 or json or xml or text
+		,parseableAs: String	// base64 or json or xml or text
 		,parsed: {}			// object to interact with data, where applicable (encoding == json | xml so far)
 		,data: String		// actual content
 	});
@@ -131,16 +109,23 @@ var Exhibition = function(){
 //
 	var xbt = new ExhibitModel();
 
-	function requestHandler.register(function(){
+	console.log(ExhibitModel);
+
+	requestHandler.register(function(req,rsp,path){
 		// PUSH A LABEL
 		// ?pushLabel=label
-		if( url.query.pushLabel ){
-			xbt.labels.push({ label: [ url.query.pushLabel ] });
+		if( path.query !== undefined && path.query.pushLabel ){
+			var label = { label: [ path.query.pushLabel ] };
+			xbt.labels.push( label );
+			xbt.save(function(err,data){
+				if(err){console.log('SHEET: '+err);}
+				else{ console.log('DATA'); console.log(data); }
+			});
 		};
 	});
-	function requestHandler.register(function(){
-		if( url.query.pushAnExhibit ){
-			xbt.exhibit.push({ labels: [ { context: 'handle' ,label: url.query.pushAnExhibit.split(/\s+/) } ] ,collection:
+	requestHandler.register(function(req,rsp,path){
+		if( path.query !== undefined && path.query.pushAnExhibit ){
+			xbt.exhibit.push({ labels: [ { context: 'handle' ,label: path.query.pushAnExhibit.split(/\s+/) } ] ,collection:
 				 [
 				 	{
 						labels: [ { context: ['coolness'],label: ['12.5'] } ]
@@ -168,14 +153,57 @@ var Exhibition = function(){
 			});
 		}
 	});
+	requestHandler.register(function(req,rsp,path){
+		// decide what to do
+		// SCRIPTS
+		if( path.pathname === '/script' || path.pathname.substr( 0 ,8 ) === '/script/' ){
+			rsp.end('/* nothing yet to see here */');
+		}
+		else
+		// STYLES
+		if( path.pathname === '/style' || path.pathname.substr( 0,7 ) === '/style/' ){
+			rsp.end('/* nothing to yet here see */');
+		}
+		else
+		// EVERYTHING ELSE
+		{
+			rsp.writeHead(
+				200
+				,{'Content-Type':'text/html'}
+			);
+			var r = [];
+
+			r.push('<!DOCTYPE html>');
+			r.push('<html lang="en">');
+			r.push('<head>');
+			r.push('<title>EXHIBITION</title>');
+			r.push('<link rel="stylesheet" type="text/css" href="/style" />');
+			r.push('<script type="text/javascript" src="/script"></script>');
+			r.push('</head>');
+			r.push('<body>');
+			r.push('<div class="exhibition">');
+			r.push('<div class="main-exhibit">');
+			
+			console.log( xbt );
+
+			r.push('</div>')
+			r.push('</div>');
+			r.push('</body>');
+			r.push('</html>');
+			
+			rsp.end( r.join('\n') );
+		};
+	});
+
 
 //
 //	HTTP SERVER
 //
-	var server = http.createServer( requestHandler );
+/*	var server = http.createServer( requestHandler.do );
 	server.listen( port ,ip );
 	console.log('Started server on '+ip+':'+port+'...');
-
+*/
+	requestHandler.createServer();
 };
 
 //
